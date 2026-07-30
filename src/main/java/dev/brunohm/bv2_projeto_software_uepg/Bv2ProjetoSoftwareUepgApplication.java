770d9112cfa0128470cdc3d98fa23f0cb9bbe4ad
brunohmdev <brunohm.dev@gmail.com>
2026-07-30 20:11:28 -0300

    first commit :))

---
+++ src/main/java/dev/brunohm/bv2_projeto_software_uepg/Bv2ProjetoSoftwareUepgApplication.java
@@ -0,0 +1,13 @@
+package dev.brunohm.bv2_projeto_software_uepg;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class Bv2ProjetoSoftwareUepgApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(Bv2ProjetoSoftwareUepgApplication.class, args);
+	}
+
+}
---
+++ src/test/java/dev/brunohm/bv2_projeto_software_uepg/Bv2ProjetoSoftwareUepgApplicationTests.java
@@ -0,0 +1,13 @@
+package dev.brunohm.bv2_projeto_software_uepg;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class Bv2ProjetoSoftwareUepgApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
